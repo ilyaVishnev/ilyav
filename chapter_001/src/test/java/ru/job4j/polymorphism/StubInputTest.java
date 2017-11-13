8434f845cd1d8ad90cef129f2c47d8dfec1604fa
ilyaVishnev <mailto:dong85@mail.ru>
2017-11-14 00:03:02 +0300

    PolymorphStubInput

---
+++ chapter_001/src/test/java/ru/job4j/polymorphism/StubInputTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.polymorphism;
+
+import ru.job4j.incapsulation.*;
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class StubInputTest {
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();     // создаём Tracker
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+        assertThat(tracker.findAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем заявку
+        Item item = tracker.add(new Item());
+        //создаём StubInput с последовательностью действий
+        Input input = new StubInput(new String[]{"2", item.getId(), "test name", "desc", "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+    }
+    @Test
+    public void whenDeleteItemsThenBeenDeleted() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test name1", "desc1");
+        Item item2 = new Item("test name2", "desc2");
+        tracker.add(item1);
+        tracker.add(item2);
+        Input input = new StubInput(new String[]{"3", tracker.findByName("test name1")[0].getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll(), arrayContainingInAnyOrder(new Item[]{item2, null}));
+
+    }
+}
