500ddae03c8b8cacfab45d18020e8a110f84ddc5
ilyaVishnev <mailto:dong85@mail.ru>
2017-11-26 14:21:04 +0300

    sorting2

--- chapter_001/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_001/src/main/java/ru/job4j/sorting/SortUser.java
@@ -10,4 +10,26 @@ public class SortUser {
         }
         return set;
     }
+
+    public static List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.name.length() - o2.name.length();
+            }
+        });
+        return list;
+    }
+
+    public static List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.name.compareTo(o2.name);
+                return result != 0 ? result : o1.age.compareTo(o2.age);
+            }
+        });
+        return list;
+    }
+
 }
