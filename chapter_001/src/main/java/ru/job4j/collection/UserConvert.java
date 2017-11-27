b21bdb14846c01168b92a829b8987f773690f740
ilyaVishnev <mailto:dong85@mail.ru>
2017-11-27 15:27:33 +0300

    UserConvert

--- chapter_001/src/main/java/ru/job4j/collection/UserConvert.java
+++ chapter_001/src/main/java/ru/job4j/collection/UserConvert.java
@@ -3,21 +3,23 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class UserConvert {
-    public static HashMap<Integer, User> process(List<User> list) {
+
+    public HashMap<Integer, User> process(List<User> list) {
         HashMap<Integer, User> map = new HashMap<Integer, User>();
-        for (int index = 0; index < list.size(); index++) {
-            map.put(list.get(index).id, list.get(index));
+        for (User user : list) {
+            map.put(user.id, user);
         }
         return map;
     }
 
     public static void main(String[] args) {
         HashMap<Integer, User> map = new HashMap<Integer, User>();
+        UserConvert userConvert = new UserConvert();
         List<User> list = new ArrayList<>();
         list.addAll(Arrays.asList(new User(1, "Ilya", "Spb"), new User(4, "Philip", "Leningrad"), new User(7, "goblin", "Moskwa")));
-        map = process(list);
-        for (Map.Entry<Integer, User> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + "  " + entry.getValue());
+        map = userConvert.process(list);
+        for (Map.Entry<Integer, User> mapp : map.entrySet()) {
+            System.out.println("ключ: " + mapp.getKey() + " значение: " + mapp.getValue());
         }
     }
 }
