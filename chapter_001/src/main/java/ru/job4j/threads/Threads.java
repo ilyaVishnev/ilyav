b21032119a1b7b2baff1727befd5d12a10d9e356
ilyaVishnev <mailto:dong85@mail.ru>
2017-12-21 23:11:52 +0300

    thread2

--- chapter_001/src/main/java/ru/job4j/threads/Threads.java
+++ chapter_001/src/main/java/ru/job4j/threads/Threads.java
@@ -2,9 +2,19 @@ package ru.job4j.threads;
 
 public class Threads {
     public static void main(String[] args) {
+        System.out.println("Начать главный поток");
         String massiv = " sdf fsdf fwefw ergrg wdwqd";
-        new Thread(new Gap(massiv)).start();
-        new Thread(new Words(massiv)).start();
+        Thread gap = new Thread(new Gap(massiv));
+        gap.start();
+        Thread worlds = new Thread(new Words(massiv));
+        worlds.start();
+        try {
+            gap.join();
+            worlds.join();
+        } catch (InterruptedException e) {
+            System.out.println("Поток прерван");
+        }
+        System.out.println("Закончить главный поток");
     }
 
     public static class Gap implements Runnable {
@@ -28,7 +38,7 @@ public class Threads {
 
     public static class Words implements Runnable {
         String example;
-        int countWord=0;
+        int countWord = 0;
 
         public Words(String example) {
             this.example = example;
@@ -37,9 +47,9 @@ public class Threads {
         @Override
         public void run() {
             String[] words = example.split("\\s");
-            for(String word:words){
-                if(!word.equals(""))
-                   countWord++;
+            for (String word : words) {
+                if (!word.equals(""))
+                    countWord++;
             }
             System.out.println("количество слов: " + countWord);
         }
