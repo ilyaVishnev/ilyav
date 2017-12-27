379321c4a781d6f8a8659bb62b593895604d3ed6
ilyaVishnev <mailto:dong85@mail.ru>
2017-12-27 16:03:13 +0300

    inter

--- chapter_001/src/main/java/ru/job4j/threads/MyInteraption.java
+++ chapter_001/src/main/java/ru/job4j/threads/MyInteraption.java
@@ -2,7 +2,7 @@ package ru.job4j.threads;
 
 public class MyInteraption {
     public static void main(String[] args) {
-        MyInteraption.CountElements countElements = new MyInteraption.CountElements("необходимое выражение условия свободы и независимости от внешних факторов и остальной фигни");
+        MyInteraption.CountElements countElements = new MyInteraption.CountElements("необходимое выражение условия свободы и независимости от внешних факторов и остальной фигниfbdfbdfbfbdfbdfbfdbdfbtbtbdfbdfbrhethrthththtrhrthtrhtrhrthtrhrthrthrthrthtrhtrhrthtrhrthtrhtrhtrhrthrthtrhtrhtrhththtr");
         MyInteraption.Waiting waiting = new MyInteraption.Waiting(countElements);
         countElements.start();
         waiting.start();
@@ -11,7 +11,6 @@ public class MyInteraption {
 
     public static class CountElements extends Thread {
         String example;
-        boolean stop = false;
 
         public CountElements(String massiv) {
             this.example = massiv;
@@ -20,8 +19,12 @@ public class MyInteraption {
         @Override
         public void run() {
             char[] elements = example.toCharArray();
-            for (int i = 0; i < elements.length && !stop; i++) {
+            for (int i = 0; i < elements.length; i++) {
                 System.out.print(elements[i]);
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Прерывание");
+                    break;
+                }
             }
         }
     }
@@ -39,7 +42,7 @@ public class MyInteraption {
             long difference;
             while (true) {
                 if ((difference = System.currentTimeMillis() - startTime) > 1) {
-                    countElement.stop = true;
+                    countElement.interrupt();
                     return;
                 }
             }
